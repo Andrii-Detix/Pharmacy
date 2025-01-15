958976da5d0e54ce892f6657bfc8f07a0e1f2878
Andrii-Detix <aivancisin6@gmail.com>
2025-01-15 23:29:55 +0200

    Add Money value object

---
+++ Pharmacy.Domain/ValueObjects/Money.cs
@@ -0,0 +1,54 @@
+﻿using Pharmacy.Domain.Abstractions.Models;
+
+namespace Pharmacy.Domain.ValueObjects;
+
+public class Money : ValueObject
+{
+    private Money(decimal amount)
+    {
+        Amount = amount;
+    }
+    public decimal Amount { get; }
+
+    public static Money Create(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new ArgumentException($"{nameof(amount)} cannot be negative");
+        }
+
+        if (!CheckDigitsCorrectness(amount))
+        {
+            throw new ArgumentException($"Incorrect {nameof(amount)}");
+        }
+        
+        return new Money(amount);
+    }
+
+    private static bool CheckDigitsCorrectness(decimal amount)
+    {
+        int numOfDigits = 2;
+
+        int multiplier = 1;
+
+        for (int i = 0; i < numOfDigits; i++)
+            multiplier *= 10;
+
+        decimal multAmount = amount * multiplier;
+        
+        return (int)multAmount == Math.Ceiling(multAmount);
+    }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Amount;
+    }
+    
+    public static Money operator +(Money a, Money b) => Create(a.Amount + b.Amount);
+    public static Money operator -(Money a, Money b) => Create(a.Amount - b.Amount);
+    public static Money operator *(Money a, int b) => Create(a.Amount * b);
+    
+    public static bool operator > (Money a, Money b) => a.Amount > b.Amount;
+    public static bool operator < (Money a, Money b) => a.Amount < b.Amount;
+    public static bool operator >= (Money a, Money b) => a.Amount >= b.Amount;
+    public static bool operator <= (Money a, Money b) => a.Amount <= b.Amount;
+}
