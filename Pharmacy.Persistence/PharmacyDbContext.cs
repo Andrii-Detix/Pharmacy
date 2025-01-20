74b82abb397f6bbe8452a7fc10537b5ca210de71
Andrii-Detix <aivancisin6@gmail.com>
2025-01-20 23:34:12 +0200

    Add DbContext

---
+++ Pharmacy.Persistence/PharmacyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Persistence.Configurations;
+
+namespace Pharmacy.Persistence;
+
+public class PharmacyDbContext : DbContext
+{
+    public PharmacyDbContext(DbContextOptions<PharmacyDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new CartConfiguration());
+        modelBuilder.ApplyConfiguration(new CartItemConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+    
+}
