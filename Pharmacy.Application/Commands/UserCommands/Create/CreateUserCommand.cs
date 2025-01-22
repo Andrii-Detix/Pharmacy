83835117b882831f82260868574a938e5ea90ae7
Andrii-Detix <aivancisin6@gmail.com>
2025-01-22 12:41:49 +0200

    Add create user command

---
+++ Pharmacy.Application/Commands/UserCommands/Create/CreateUserCommand.cs
@@ -0,0 +1,5 @@
+﻿using Pharmacy.Application.Abstractions.Commands;
+
+namespace Pharmacy.Application.Commands.UserCommands.Create;
+
+public record CreateUserCommand(string Name, string Email, string Password) : ICommand<Guid>;
---
+++ Pharmacy.Application/Commands/UserCommands/Create/CreateUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Application.Abstractions;
+using Pharmacy.Application.Abstractions.Commands;
+using Pharmacy.Domain.Entities;
+
+namespace Pharmacy.Application.Commands.UserCommands.Create;
+
+public class CreateUserHandler(IPharmacyDbContext _context) : ICommandHandler<CreateUserCommand, Guid>
+{
+    public async Task<Guid> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+    {
+        var user = User.Create(Guid.NewGuid(), command.Name, command.Email, command.Password);
+
+        bool isExist = await _context.Users.AnyAsync(x => x.Email == user.Email, cancellationToken);
+        
+        if (isExist)
+        {
+            throw new ArgumentException($"User with email {user.Email} already exists.");
+        }
+        
+        await _context.Users.AddAsync(user, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return user.Id;
+    }
+}
