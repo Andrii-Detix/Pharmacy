f5c615f191f2d8bcdd30c5f5d1ef81d3df4859e1
Andrii-Detix <aivancisin6@gmail.com>
2025-01-22 15:53:31 +0200

    Add User endpoints

---
+++ Pharmacy.Web/Extensions/EndPoints/UserEndPointsExtension.cs
@@ -0,0 +1,92 @@
+﻿using MediatR;
+using Pharmacy.Application.Commands.UserCommands.Create;
+using Pharmacy.Application.Commands.UserCommands.UpdateName;
+using Pharmacy.Application.Queries.UserQueries.GetByEmail;
+using Pharmacy.Application.Queries.UserQueries.GetById;
+
+namespace Pharmacy.Web.Extensions.EndPoints;
+
+public static class UserEndPointsExtension
+{
+    public static void MapUserEndPoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.CreateUser();
+        endpoints.GetUserById();
+        endpoints.GetUserByEmail();
+        endpoints.UpdateUserName();
+    }
+
+    private static void CreateUser(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("/users", async (ISender sender, CreateUserCommand user) =>
+        {
+            try
+            {
+                Guid id = await sender.Send(user);
+
+                return Results.Ok(id);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+    }
+
+    private static void GetUserById(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/users/{id:guid}", async (ISender sender, Guid id) =>
+        {
+            try
+            {
+                var query = new GetUserByIdQuery(id);
+
+                var user = await sender.Send(query);
+
+                return Results.Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+    }
+
+    private static void GetUserByEmail(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/users", async (ISender sender, string email) =>
+        {
+            try
+            {
+                var query = new GetUserByEmailQuery(email);
+
+                var user = await sender.Send(query);
+
+                return Results.Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+    }
+
+    private static void UpdateUserName(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPut("/users/{id:guid}", async (ISender sender, Guid id, string name) =>
+        {
+            try
+            {
+                var command = new UpdateUserNameCommand(id, name);
+
+                await sender.Send(command);
+
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+    }
+}
