471abef4461d085659e895113ad434e3d27b7550
Andrii-Detix <aivancisin6@gmail.com>
2025-01-22 12:42:28 +0200

    Add get user by email query

---
+++ Pharmacy.Application/Queries/GetByEmail/GetUserByEmailHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacy.Application.Abstractions;
+using Pharmacy.Application.Abstractions.Queries;
+using Pharmacy.Domain.Entities;
+using Pharmacy.Domain.ValueObjects;
+
+namespace Pharmacy.Application.Queries.GetByEmail;
+
+public class GetUserByEmailHandler(IPharmacyDbContext _context) : IQueryHandler<GetUserByEmailQuery, User>
+{
+    public async Task<User> Handle(GetUserByEmailQuery query, CancellationToken cancellationToken)
+    {
+        var email = Email.Create(query.Email);
+        
+        var user = await _context.Users.AsNoTracking()
+            .Where(u => u.Email == email)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (user is null)
+        {
+            throw new Exception($"User with email {query.Email} does not exist");
+        }
+        
+        return user;
+    }
+}
---
+++ Pharmacy.Application/Queries/GetByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,6 @@
+﻿using Pharmacy.Application.Abstractions.Queries;
+using Pharmacy.Domain.Entities;
+
+namespace Pharmacy.Application.Queries.GetByEmail;
+
+public record GetUserByEmailQuery(string Email) : IQuery<User>;
