813d16c354b836c2cfca7ef9a04a6410d7144531
Andrii-Detix <aivancisin6@gmail.com>
2025-01-24 20:55:13 +0200

    update endpoint extensions

---
+++ Pharmacy.Web/Extensions/EndPoints/EndPointsExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Pharmacy.Web.Extensions.EndPoints;
+
+public static class EndPointsExtension
+{
+    public static void MapEndPoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapUserEndPoints();
+        endpoints.MapProductEndPoints();
+    }
+}
