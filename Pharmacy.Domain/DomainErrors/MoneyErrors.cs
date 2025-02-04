dc8f37f48982601379ad0450e4df17676d6c1ed9
Andrii-Detix <aivancisin6@gmail.com>
2025-02-04 17:41:30 +0200

    Add Domain Errors

---
+++ Pharmacy.Domain/DomainErrors/MoneyErrors.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Errors;
+
+namespace Pharmacy.Domain.DomainErrors;
+
+public static class MoneyErrors
+{
+    public static Error NegativeMoneyAmount => Error.Validation("Money.NegativeAmount", "Amount of money cannot be negative");
+    
+    public static Error InvalidMoney => Error.Validation("Money.Invalid", "Money is invalid");
+}
---
+++ Pharmacy.Domain/DomainErrors/UserErrors.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Errors;
+
+namespace Pharmacy.Domain.DomainErrors;
+
+public static class UserErrors
+{
+    public static Error EmptyId => Error.Validation("User.Id.Empty", "The user id is empty");
+    
+    public static Error InvalidName => Error.Validation("User.Name.Invalid", "The user name is invalid");
+    
+    public static Error SameName => Error.Conflict("User.Name.Same", "The user name is same");
+}
